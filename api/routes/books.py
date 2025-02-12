c8e11a6001a7a92466d20cd760b9906bed5f5157
Kayce-joy <wakarijoyce@gmail.com>
2025-02-12 22:05:44 +0300

    feat(api): Implemented missing /api/v1/books/{book_id} endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -32,6 +32,12 @@ db.books = {
     ),
 }
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.books.get(book_id)
+    if book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
